d0dada3b330d50334bff79b16969f2e5ae4bace6
aumnitipath <aumnitipath@outlook.com>
2023-09-13 15:42:47 +0700

    feat:add start sever backend

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Iphone 12 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Iphone 12 ",
+    desc: "5.4-inch display",
+    price: 699,
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5-inch display",
+    price: 399,
+    image: "",
+  },
+];
+
+module.exports = products;

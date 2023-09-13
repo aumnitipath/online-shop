35b7b2e60e194f55934836f3637adab8ffd8ffea
aumnitipath <aumnitipath@outlook.com>
2023-09-13 21:22:35 +0700

    feat:add style cart items

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,20 @@ import App from "./App.jsx";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-import productsReducer from "./features/productsSlice.js";
+import productsReducer, { productsFetch } from "./features/productsSlice.js";
+import { productsApi } from "./features/productsAPI.js";
 
 const store = configureStore({
   reducer: {
-    products:productsReducer,
+    products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(productsApi.middleware);
   },
 });
+
+store.dispatch(productsFetch());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

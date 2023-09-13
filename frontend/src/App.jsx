08ca5e7b774e714dd8638859b2843e04814b8e0b
aumnitipath <aumnitipath@outlook.com>
2023-09-13 13:53:38 +0700

    add landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import NotFound from "./components/Notfound";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

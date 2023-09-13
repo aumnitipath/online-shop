ab0ff5f8a30a8f83f5e1890a73d82f879e989d9c
aumnitipath <aumnitipath@outlook.com>
2023-09-13 17:43:53 +0700

    feat:add redux toolkit for shopSlice

--- backend/products.js
+++ backend/products.js
@@ -4,21 +4,24 @@ const products = [
     name: "Iphone 12 Pro",
     desc: "6.1-inch display",
     price: 999,
-    image: "",
+    image:
+      "https://res.cloudinary.com/aumntp/image/upload/v1694596654/online-shop-project/iphone12pro_objpin.jpg",
   },
   {
     id: 2,
     name: "Iphone 12 ",
     desc: "5.4-inch display",
     price: 699,
-    image: "",
+    image:
+      "https://res.cloudinary.com/aumntp/image/upload/v1694596654/online-shop-project/iphone12_pgm8bl.jpg",
   },
   {
     id: 3,
     name: "Galaxy S",
     desc: "6.5-inch display",
     price: 399,
-    image: "",
+    image:
+      "https://res.cloudinary.com/aumntp/image/upload/v1694596653/online-shop-project/galaxyS_f7njkw.png",
   },
 ];
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import productsReducer from "./features/productsSlice.js";
+
+const store = configureStore({
+  reducer: {
+    products:productsReducer,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

086713180b78846f247718579860f874f8de0360
aumnitipath <aumnitipath@outlook.com>
2023-09-14 18:56:40 +0700

    connect with mongoDB

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const products = require("./products");
 
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -19,5 +19,14 @@ app.get("/products", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
 
 app.listen(port, console.log(`Server running on Port ${port}`));
+
+mongoose
+  .connect(uri, {
+    useNewURlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection successful..."))
+  .catch((err) => console.log("MongoDB connection failed", err.message));

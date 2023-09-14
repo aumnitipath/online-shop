b05a64e1872dd549222e4a494e7abc1497c19d89
aumnitipath <aumnitipath@outlook.com>
2023-09-14 18:36:11 +0700

    add package mongoose and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const products = require("./products");
 
 const app = express();
+require("dotenv").config()
 
 app.use(express.json());
 app.use(cors());
